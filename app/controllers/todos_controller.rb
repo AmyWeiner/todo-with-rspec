3d3ba33dac948cc83617924fdf4ecdcd81c453f2
Amy Weiner <arweiner@alumni.stanford.edu>
2014-02-10 17:13:16 -0800

    resolves conflicts

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
-<<<<<<< HEAD
+
   def new
   	@todo = Todo.new
   end
@@ -10,15 +10,5 @@ class TodosController < ApplicationController
   	redirect_to "/todos/new"
   end
 end
-=======
-	def new
-		@todo = Todo.new
-	end
 
-	def create
-		title_param = params[:todo].permit(:title)
-		@todo = Todo.create title_param
-		redirect_to "/todos/new"
-	end
-end
->>>>>>> 3093f8646c0c749ecbe28f1fa9a234b587e8d2dd
+	
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 TodoWithRspec::Application.routes.draw do
 
-  
   	get "todos/new", to: 'todos#new', as: 'new_todo'
 
 	post '/todos', to: 'todos#create'
